af02f5e21c6f4447f2fd5c6410843563e3fdf3c9
SaishBafna <151601437+SaishBafna@users.noreply.github.com>
2026-03-10 15:56:38 +0530

    Bugs solved and need to update code

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import HRMSDashboard from "./pages/Dashboard";
 import EmployeeManagement from "./pages/Employees";
 import IncentiveRecords from "./pages/Incentives";
 import Recruitment from "./pages/Recruitement";
-import AttendanceShiftManagement from "./pages/Attendance";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AttendanceShiftManagement from "./pages/AttendanceShiftManagement";
 import PayrollManagement from "./pages/Payroll";
 import LeaveManagement from "./pages/LeaveManagement";
 import PerformanceManagement from "./pages/PerformanceManagement";
@@ -15,29 +14,40 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-<BrowserRouter>
-  <SidebarLayout>
-    <Routes>
-      <Route path="/hrms/dashboard" element={<HRMSDashboard />} />
-      <Route path="/hrms/employees" element={<EmployeeManagement />} />
-      <Route path="/hrms/incentives" element={<IncentiveRecords />} />
-      <Route path="/hrms/recruitment" element={<Recruitment />} />
-      <Route path="/hrms/attendance" element={<AttendanceShiftManagement />} />
-      <Route path="/hrms/payroll" element={<PayrollManagement />} />
-      <Route path="/hrms/leave" element={<LeaveManagement />} />
-      <Route path="/hrms/performance" element={<PerformanceManagement />} />
-            <Route path="/hrms/performance" element={<PerformanceManagement />} />
-                        <Route path="/hrms/calender" element={<CalenderTraining />} />
-                        <Route path="/hrms/compilance" element={<Compilance />} />
+    <BrowserRouter>
+      <SidebarLayout>
+        <Routes>
+          <Route path="/hrms/dashboard" element={<HRMSDashboard />} />
+          <Route path="/hrms/employees" element={<EmployeeManagement />} />
+          <Route path="/hrms/incentives" element={<IncentiveRecords />} />
+          <Route path="/hrms/recruitment" element={<Recruitment />} />
+          <Route
+            path="/hrms/attendance"
+            element={<AttendanceShiftManagement />}
+          />
+          <Route path="/hrms/payroll" element={<PayrollManagement />} />
+          <Route path="/hrms/leave" element={<LeaveManagement />} />
+          <Route path="/hrms/performance" element={<PerformanceManagement />} />
+          <Route path="/hrms/performance" element={<PerformanceManagement />} />
+          <Route path="/hrms/calender" element={<CalenderTraining />} />
+          <Route path="/hrms/compilance" element={<Compilance />} />
+         
+          {/* Payroll Routes */}
+          <Route path="/hrms/payroll" element={<PayrollManagement />} />
+          <Route path="/hrms/payroll/salary" element={<PayrollManagement />} />
+          <Route path="/hrms/payroll/processing" element={<PayrollManagement />} />
+          <Route path="/hrms/payroll/incentive" element={<PayrollManagement />} />
+          <Route path="/hrms/payroll/payslip" element={<PayrollManagement />} />
+          <Route path="/hrms/payroll/deductions" element={<PayrollManagement />} />
+          <Route path="/hrms/payroll/reports" element={<PayrollManagement />} />
+          <Route path="/hrms/payroll/settings" element={<PayrollManagement />} />
 
-
-
-
-
-
-    </Routes>
-  </SidebarLayout>
-</BrowserRouter>
-  )
+          {/* Default redirect */}
+          <Route path="/" element={<Navigate to="/hrms/dashboard" replace />} />
+        
+        </Routes>
+      </SidebarLayout>
+    </BrowserRouter>
+  );
 }
-export default App
+export default App;
